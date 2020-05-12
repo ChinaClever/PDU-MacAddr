6e5eb7acb92a2aa2fd6adbe04f81772aa0e41d9d
luozhiyong <luozhiyong131@qq.com>
2020-05-12 11:06:12 +0800

    a

--- MacAddr/main.cpp
+++ MacAddr/main.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.h"
-
+#include <QMessageBox>
 #include <QApplication>
+#include "QtSingleApplication.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QtSingleApplication a(QLatin1String("MacAddr"),argc, argv);
+    if(a.isRunning())  {
+        QMessageBox::information(NULL, QObject::tr("提示"), QObject::tr("程序已运行！"),QMessageBox::Ok);
+        a.sendMessage("raise_window_noop", 1000); //4s后激活前个实例
+        return EXIT_SUCCESS;
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
